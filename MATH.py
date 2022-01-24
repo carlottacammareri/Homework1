f8e30068a02907288b29ea383c227ce491963708
carlottacammareri <92098700+carlottacammareri@users.noreply.github.com>
2022-01-24 10:46:22 +0100

    Add files via upload

---
+++ MATH.py
@@ -0,0 +1,29 @@
+#homework1 CARLOTTA CAMMARERI
+#MATH
+
+#TRIANGLE QUEST 2
+
+for i in range(1,int(input())+1):
+    print ((10**i//9)**2)
+
+#MOD DIVMOD
+
+a = int(input());
+b= int(input());
+print(a//b);
+print(a%b);
+print(divmod(a,b));
+
+#POWER MOD POWER
+
+a=int(input())
+b=int(input())
+m=int(input())
+
+print(pow(a,b))
+print(pow(a,b,m))
+
+#TRIANGLE QUEST
+
+for i in range(1,int(input())): 
+    print(int(i * 10**i/9))
